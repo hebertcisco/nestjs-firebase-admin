15730472d552637d3f859bb3926ffd7ea8661b3c
hebertcisco <mrrabbitomg@gmail.com>
2023-04-08 23:32:20 -0300

    update app.constant.ts

--- examples/nodejs/nestjs/typescript/src/app.constant.ts
+++ examples/nodejs/nestjs/typescript/src/app.constant.ts
@@ -1,5 +1,11 @@
 import { configService } from 'nest-shared';
 
-export const FIREBASE_ADMIN_PROJECT_ID = configService.getValue('FIREBASE_ADMIN_PROJECT_ID');
-export const FIREBASE_ADMIN_CLIENT_EMAIL = configService.getValue('FIREBASE_ADMIN_CLIENT_EMAIL');
-export const FIREBASE_ADMIN_PRIVATE_KEY = configService.getValue('FIREBASE_ADMIN_PRIVATE_KEY');
+export const FIREBASE_ADMIN_PROJECT_ID = configService.getValue<string>(
+  'FIREBASE_ADMIN_PROJECT_ID',
+);
+export const FIREBASE_ADMIN_CLIENT_EMAIL = configService.getValue<string>(
+  'FIREBASE_ADMIN_CLIENT_EMAIL',
+);
+export const FIREBASE_ADMIN_PRIVATE_KEY = configService.getValue<string>(
+  'FIREBASE_ADMIN_PRIVATE_KEY',
+);
