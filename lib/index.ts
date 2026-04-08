4639a69b85ba055db5fec12efbf921f4db4cf059
Hebert <hebertcisco@outlook.com>
2026-04-08 19:47:53 -0300

    fix: export auth.service from index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 export * from './modules/admin/admin.module';
 export * from './modules/admin/services/admin.service';
+export * from './modules/admin/services/auth.service';
 export * from './modules/admin/services/database.service';
 export * from './modules/admin/services/messaging.service';
 export * from './modules/admin/services/firestore.service';
